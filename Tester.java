aee397f3aef89c5f9cf489b150fdde04e60e32ba
wdong20 <wdong20@stuy.edu>
2020-12-16 12:11:44 -0500

    Added helper methods for testing purposes

--- Tester.java
+++ Tester.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class Tester {
     public static void main(String[] args) {
+        /*
+        For more clarity
+        ✔ - Correct
+        ✘ - Very no good
+        */
+
         
+    }
+
+    public static void printGood(String comment) {
+        System.out.println("✔ " + comment);
+    }
+
+    public static void printBad(char section) {
+        System.out.println("✘ Whoops! Something went wrong in section " + section);
+    }
+
+    public static void printSection(char section) {
+        System.out.println("\nSection " + section + ":");
     }
 }
